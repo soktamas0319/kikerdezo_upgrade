4a417787222ee7143209bd0743ad7bfc3a5c6c22
Sok Tamás <soktamas@yahoo.com>
2021-10-14 21:00:56 +0200

    Jav3

--- res/engine.js
+++ res/engine.js
@@ -1,6 +1,6 @@
 // First Level Items MouseOver Style
-function chgPageStl(OverStyle,divID) {
-	if (OverStyle==1){
+function chgPageStl(OverStyle, divID) {
+	if (OverStyle == 1) {
 		document.all["ID_elpage" + divID].style.backgroundColor = mainOverCol;
 		document.all["ID_elpage" + divID].style.color = mainFntOverCol;
 	}
